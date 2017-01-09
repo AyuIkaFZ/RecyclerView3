5636b59e8e1995fca623b3a52da13781fafd3a88
AyuIkaFZ <ayuika116@gmail.com>
2017-01-09 11:35:57 +0700

    RecyclerView View Detail

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -1,8 +1,8 @@
 package id.sch.smktelkom_mlg.learn.recyclerview2;
 
+import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -37,13 +37,25 @@ public class MainActivity extends AppCompatActivity {
         Resources resources = getResources();
         String[] arJudul = resources.getStringArray(R.array.places);
         String[] arDeskripsi = resources.getStringArray(R.array.place_desc);
+        String[] arDetail = resources.getStringArray(R.array.place_details);
+        String[] arLokasi = resources.getStringArray(R.array.place_locations);
         TypedArray a = resources.obtainTypedArray(R.array.places_picture);
-        Drawable[] arFoto = new Drawable[a.length()];
+        String[] arFoto = new String[a.length()];
         for (int i = 0; i < arFoto.length; i++) {
-            arFoto[i] = a.getDrawable(i);
-        }
+            int id = a.getResourceId(i, 0);
 
+            arFoto[i] = ContentResolver.SCHEME_ANDROID_RESOURCE + "://"
+                    + resources.getResourcePackageName(id) + '/'
+                    + resources.getResourceTypeName(id) + '/'
+                    + resources.getResourceEntryName(id);
+        }
         a.recycle();
+
+        for (int i = 0; i < arJudul.length; i++) {
+            mList.add(new Hotel(arJudul[i], arDeskripsi[i], arDetail[i], arLokasi[i], arFoto[i]));
+
+        }
+        mAdapter.notifyDataSetChanged();
 
 
     }
--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/adapter/HotelAdapter.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/adapter/HotelAdapter.java
@@ -1,5 +1,8 @@
 package id.sch.smktelkom_mlg.learn.recyclerview3.adapter;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +12,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import id.sch.smktelkom_mlg.learn.recyclerview3.DetailActivity;
 import id.sch.smktelkom_mlg.learn.recyclerview3.R;
 import id.sch.smktelkom_mlg.learn.recyclerview3.model.Hotel;
 
@@ -19,8 +23,11 @@ import id.sch.smktelkom_mlg.learn.recyclerview3.model.Hotel;
 public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder> {
     ArrayList<Hotel> hotelList;
 
-    public HotelAdapter(ArrayList<Hotel> hotelList) {
+    IHotelAdapter mIHotelAdapter;
+
+    public HotelAdapter(Context context, ArrayList<Hotel> hotelList) {
         this.hotelList = hotelList;
+        mIHotelAdapter = (IHotelAdapter) context;
     }
 
     @Override
@@ -28,6 +35,8 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder> 
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, parent, false);
         ViewHolder vh = new ViewHolder(v);
         return vh;
+
+        HotelAdapter mAdapter = new HotelAdapter(this, mList);
     }
 
     @Override
@@ -35,7 +44,7 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder> 
         Hotel hotel = hotelList.get(position);
         holder.tvJudul.setText(hotel.judul);
         holder.tvDeskripsi.setText(hotel.deskripsi);
-        holder.ivFoto.setImageDrawable(hotel.foto);
+        holder.ivFoto.setImageURI(Uri.parse(hotel.foto));
 
     }
 
@@ -44,6 +53,16 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder> 
         if (hotelList != null)
             return hotelList.size();
         return 0;
+    }
+
+    public interface IHotelAdapter {
+        Intent intent = new Intent(this, DetailActivity.class);
+
+        startActivity(intent);
+
+        intent.putExtra(HOTEL,mlist.get(pos))
+
+        void doClick(int pos);
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -57,6 +76,13 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder> 
             ivFoto = (ImageView) itemView.findViewById(R.id.imageView);
             tvJudul = (TextView) itemView.findViewById(R.id.textViewJudul);
             tvDeskripsi = (TextView) itemView.findViewById(R.id.textViewDeskripsi);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mIHotelAdapter.doClick(getAdapterPosition());
+                }
+            });
         }
     }
 }
